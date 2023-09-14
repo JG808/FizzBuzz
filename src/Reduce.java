1b8d96bb6b13635f80147b5d13e435196d1d6796
Jia Gu <jjia.gu@mail.utoronto.ca>
2023-09-13 22:48:02 -0400

    new function Reduce

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 90;
+        int k = 0;
+        while (n > 0) {
+            k++;
+            if (n % 2 == 0){
+                n = n / 2;
+            } else n--;
+        }
+        System.out.println(k);
+    }
 }
